c3b2d700ab46c59bdcec16ead07f8cd7cf2b4384
jonchongch <54684314+jonchongch@users.noreply.github.com>
2024-11-09 23:36:24 +0800

    cv-html-update-1002

--- js/print-adjustment.js
+++ js/print-adjustment.js
@@ -1,24 +1,28 @@
-// Function to add padding dynamically if an element is about to break onto the next page
 function addDynamicPaddingOnPageBreak() {
    const sections = document.querySelectorAll('.section, .list-group-item');
-   
+
    sections.forEach(section => {
-      // Temporarily clone the section and append it to the body to simulate its page break behavior
-      const clone = section.cloneNode(true);
-      document.body.appendChild(clone);
-      
-      // Calculate if the section will fit on the current page
-      const pageHeight = window.innerHeight; // Height of the page
-      const sectionHeight = clone.offsetHeight; // Height of the section
-      const position = clone.getBoundingClientRect(); // Get its position
+       // Skip the first child of the .list-group
+       if (section.classList.contains('list-group-item') && section === section.parentNode.firstElementChild) {
+           return;
+       }
 
-      // Check if the section will go off the page
-      if (position.bottom > pageHeight) {
-         section.classList.add('py-025'); // Add the padding if the section is too long
-      }
+       // Temporarily clone the section and append it to the body to simulate its page break behavior
+       const clone = section.cloneNode(true);
+       document.body.appendChild(clone);
 
-      // Remove the clone after the check
-      clone.remove();
+       // Calculate if the section will fit on the current page
+       const pageHeight = window.innerHeight; // Height of the page
+       const sectionHeight = clone.offsetHeight; // Height of the section
+       const position = clone.getBoundingClientRect(); // Get its position
+
+       // Check if the section will go off the page
+       if (position.bottom > pageHeight) {
+           section.classList.add('py-025'); // Add the padding if the section is too long
+       }
+
+       // Remove the clone after the check
+       clone.remove();
    });
 }
 
